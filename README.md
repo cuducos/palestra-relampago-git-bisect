a4c47f7ac39f77c3b2487c8e6e0a8cc496b46808
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-10-14 11:36:19 -0400

    Adiciona forma de passar a expressão pelo terminal

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-Exemplo de uso:
+# Exemplo de uso
+
+## Modo interativo
 
 ```console
 $ python -m calculadora
 => 40 + 2
 42
 ```
+
+## Passando uma expressão
+
+```console
+$ python -m calculadora --expressão "40 + 2" 
+42
+```
+
+A opção `--expressão` pode ser abreviada como `-e`.
--- calculadora/__main__.py
+++ calculadora/__main__.py
@@ -1,10 +1,12 @@
-from click import secho, prompt
+from click import command, option, secho, prompt
 
 from calculadora.parser import ParserError, parse
 
 
-def pergunta():
-    expressão = prompt("=>", prompt_suffix=" ")
+def pergunta(expressão=None):
+    if not expressão:
+        expressão = prompt("=>", prompt_suffix=" ")
+
     try:
         return parse(expressão)
     except ParserError as error:
@@ -13,9 +15,11 @@ def pergunta():
     return pergunta()
 
 
-def cli():
+@command()
+@option("-e", "--expressão")
+def cli(expressão=None):
     """Calculadora na sua linha de comando"""
-    função, x, y = pergunta()
+    função, x, y = pergunta(expressão)
     resultado = função(x, y)
     secho(str(resultado), fg="green")
 

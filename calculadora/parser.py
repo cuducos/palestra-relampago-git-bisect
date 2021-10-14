5985e5177b9e00a2ed748942e151c4ed68e98f5a
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-10-14 11:24:07 -0400

    Cria parser de expressões

---
+++ calculadora/parser.py
@@ -0,0 +1,24 @@
+from re import compile, match
+
+from calculadora import adição
+
+REGEX = compile(r"(?P<x>\d+)\s?(?P<função>[+\-*/])\s?(?P<y>\d+)")
+MAPPING = {"+": adição}
+
+
+class ParserError(Exception):
+    pass
+
+
+def parse(expressão):
+    matches = match(REGEX, expressão)
+    if not matches:
+        raise ParserError(f"{expressão} não é uma expressão válida")
+
+    x, y = int(matches.group("x")), int(matches.group("y"))
+    sinal = matches.group("função")
+    função = MAPPING[sinal]
+    if not função:
+        raise ParserError(f"{sinal} não é um sinal válido")
+
+    return função, x, y
